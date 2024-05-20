995fdf31f72332ac5d0fb058b58bd47d9df37fc8
Jorge Gabriel <105565673+jorgegabrielsl@users.noreply.github.com>
2024-05-20 19:47:15 -0300

    Update algoritmo.cpp

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -1,42 +1,46 @@
-  #include <bits/stdc++.h>
-  using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-  #define V 4
-  #define INF 99999
+#define V 4 // Número de vértices no grafo
+#define INF 99999 // Definir um valor para representar a ausência de caminho direto entre dois vértices.
 
-  void printSolution(int dist[][V]);
+void imprimirSolucao(int dist[][V]); // Declaração da função para imprimir a solução final
 
-  void floydWarshall(int dist[][V]) {
-      int i, j, k;
-      for (k = 0; k < V; k++) {
-          for (i = 0; i < V; i++) {
-              for (j = 0; j < V; j++) {
-                  if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] != INF && dist[i][k] != INF))
-                      dist[i][j] = dist[i][k] + dist[k][j];
-              }
-          }
-      }
-      printSolution(dist);
-  }
+void floydWarshall(int dist[][V]) {
+    int i, j, k;
+    // Laços atrelados para encontrar os caminhos mais curtos
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                // Se o vértice k está no caminho mais curto de i para j, atualize dist[i][j]
+                if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] != INF && dist[i][k] != INF))
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    imprimirSolucao(dist); // Chama a função para imprimir a solução final
+}
 
-  void printSolution(int dist[][V]) {
-      cout << "The following matrix shows the shortest distances between every pair of vertices\n";
-      for (int i = 0; i < V; i++) {
-          for (int j = 0; j < V; j++) {
-              if (dist[i][j] == INF)
-                  cout << "INF" << " ";
-              else
-                  cout << dist[i][j] << "   ";
-          }
-          cout << endl;
-      }
-  }
+void imprimirSolucao(int dist[][V]) {
+    // Imprime a matriz de distâncias mais curtas
+    cout << "A matriz a seguir mostra as distâncias mais curtas entre cada par de vértices\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF" << " ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
 
-  int main() {
-      int graph[V][V] = { { 0, 5, INF, 10 },
-                          { INF, 0, 3, INF },
-                          { INF, INF, 0, 1 },
-                          { INF, INF, INF, 0 } };
-      floydWarshall(graph);
-      return 0;
-  }
+int main() {
+    // Matriz de adjacência do grafo de entrada
+    int grafo[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+    floydWarshall(grafo); // Executa o algoritmo de Floyd-Warshall
+    return 0;
+}
